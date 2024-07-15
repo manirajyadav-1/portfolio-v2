ceab86bd89b7f66b2e3eae31fb847280e52804e3
manirajyadav-1 <yadavmani41@gmail.com>
2024-07-15 22:50:39 +0545

    Fixed header issue

--- components/Head/index.js
+++ components/Head/index.js
@@ -5,11 +5,11 @@ export default function DomHead({ pageName = "HomePage" }) {
     return (
         <Head>
             <title>Maniraj Yadav -{pageName} </title>
-            {/* meta tags end
+            {/* meta tags end */}
             <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
             <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
             <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"></link>
-            <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script> */}
+            <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
         </Head>
     )
 }
